41e7bf874f6458beadfd03c714b19289f3ed3111
Karthick0812 <144112326+Karthick0812@users.noreply.github.com>
2025-07-07 11:37:56 +0530

    Update secondbig.c

--- secondbig.c
+++ secondbig.c
@@ -1,28 +1,48 @@
 #include<stdio.h>
 int main()
-
 {
-    int big1,big2,i,a[5]={9,8,9,0,3};
-    if(a[0]>a[1])
-    {
-        big1=a[0];
-        big2=a[1];
-    }
-else    {
-        big1=a[1];
-        big2=a[0];
-    }
-    for(i=2;i<5;i++)
-    {
-        if(big1<a[i])
-        {
-            big2=big1;
-            big1=a[i];
-        }
-        else if((big2<a[i])&&(a[i]!=big1))
-        {
-            big2=a[i];
-        }
-    }
-    printf("%d\n",big2);
+	int a[8];
+	int i,big1,big2,n=sizeof a/sizeof a[0];
+
+	printf("Enter the array elements\n");
+	
+	for(int i=0;i<n;i++)
+	scanf("%d",&a[i]);
+
+
+	for(i=0;i<n-1;i++)
+	{
+		if(a[i]!=a[i+1])
+				break;
+	}
+if(i != n-1)
+{
+
+	if(a[i]>a[i+1])
+	{
+	big1=a[i];
+	big2=a[i+1];
+	}
+	else
+	{
+		big1=a[i+1];
+		big2=a[i];
+	}
+
+	for(i=i+2;i<n;i++)
+	{
+		if(a[i]>big1)
+		{
+			big2=big1;
+			big1=a[i];
+		}
+		else if((a[i]>big2)&&(a[i]!=big1))
+		big2=a[i];
+
+	}
+
+	printf("Biggest Number=%d \nSecond Biggest Number=%d\n",big1,big2);
+}
+else
+printf("All are same\n");
 }
